3801636ada9769ae7c66c565a02b2681a69bb9ab
Leopiron <sourovsarkar84@gmail.com>
2019-10-10 22:44:16 +0530

    Single Linear Regression in R Added

---
+++ ML/Single Linear Regression/data_preprocessing.R
@@ -0,0 +1,8 @@
+dataset = read.csv('Dataset.csv')
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
