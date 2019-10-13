90864879585fa08a586c7aaa9ccf2646c4c00331
Leopiron <sourovsarkar84@gmail.com>
2019-10-13 22:39:30 +0530

    Dataset Added

--- ML/Single Linear Regression/data_preprocessing.R
+++
@@ -1,8 +0,0 @@
-dataset = read.csv('Dataset.csv')
-
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
-
