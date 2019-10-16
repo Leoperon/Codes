c55189d7b77c3ebfeae206c2c9630406ac40cebc
Leopiron <sourovsarkar84@gmail.com>
2019-10-16 23:52:33 +0530

    MLR notebook Added

--- ML/Multiple Linear Regression/Multiple_linear_regression.R
+++ ML/Multiple Linear Regression/Multiple_linear_regression.R
@@ -8,4 +8,7 @@ set.seed(123)
 split = sample.split(dataset$Profit, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
+regressor = lm(formula = Profit ~ .,
+               data = training_set)
 
+y_pred = predict(regressor, newdata = test_set)
