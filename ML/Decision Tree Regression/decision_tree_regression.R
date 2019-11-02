d81fbbbbe246331f36678cf44570bfe70b4d6a56
Leopiron <sourovsarkar84@gmail.com>
2019-11-02 23:48:11 +0530

    Added

---
+++ ML/Decision Tree Regression/decision_tree_regression.R
@@ -0,0 +1,33 @@
+# Decision Tree Regression
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+training_set = scale(training_set)
+test_set = scale(test_set)
+
+install.packages('rpart')
+library(rpart)
+regressor = rpart(formula = Salary ~ .,
+                  data = dataset,
+                  control = rpart.control(minsplit = 1))
+
+y_pred = predict(regressor, data.frame(Level = 6.5))
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Decision Tree Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+plot(regressor)
+text(regressor)
