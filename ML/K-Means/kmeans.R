ea607daf2702a7ab05ed31515314ae1bde16e292
Leopiron <sourovsarkar84@gmail.com>
2019-12-14 06:11:41 +0530

    R File Added

---
+++ ML/K-Means/kmeans.R
@@ -0,0 +1,27 @@
+        # K-Means Clustering
+dataset = read.csv('Mall_Customers.csv')
+dataset = dataset[4:5]
+set.seed(6)
+wcss = vector()
+for (i in 1:10) wcss[i] = sum(kmeans(dataset, i)$withinss)
+plot(1:10,
+     wcss,
+     type = 'b',
+     main = paste('The Elbow Method'),
+     xlab = 'Number of clusters',
+     ylab = 'WCSS')
+set.seed(29)
+kmeans = kmeans(x = dataset, centers = 5)
+y_kmeans = kmeans$cluster
+library(cluster)
+clusplot(dataset,
+         y_kmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of customers'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
